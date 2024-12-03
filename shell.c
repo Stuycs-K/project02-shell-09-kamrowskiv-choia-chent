f744115e61797224d7e2c4363798688153437463
Tony Chen <tchen51@stuy.edu>
2024-12-03 13:51:39 -0500

    add working reading commands

--- shell.c
+++ shell.c
@@ -13,36 +13,43 @@
 #include "shell.h"
 
 int main() {
-    char input[256];
-    char cwd[256];
-    getcwd(cwd, 256);
-    printf("%s $ ", cwd);
-    while (fgets(input, 256, stdin)) {
-        char withoutnl[256];
-        sscanf(input, "%[^\n]", withoutnl);
-        char * args[20];
-        parse_args(withoutnl, args);
+
+    while (1) {
+        char cwd[256];
+        getcwd(cwd, 256);
+        printf("%s $ ", cwd);
+        
+        char input[256];
+        fgets(input, 256, stdin);
+        char in[256];
+        sscanf(input, "%[^\n]", in);
+        char * args[200];
+        parse_args(in, args);
+
         pid_t p = fork();
+
         if (p < 0) {
             perror("fork fail");
             exit(1);
-        } else if (p == 0) {
+        }
+        else if (p == 0) {
             execvp(args[0], args);
+            exit(1);
         }
         else {
             int status;
             int id = wait(&status);
-            printf("%s $ ", cwd);
         }
 
-        
     }
+
 }
 
-void parse_args(char line[256], char * arg_ary[20]) {
+void parse_args(char line[256], char * arg_ary[200]) {
     int i = 0;
     while (line) {
         arg_ary[i] = strsep(&line, " ");
         i++;
     }
+    arg_ary[i] = 0;
 }
